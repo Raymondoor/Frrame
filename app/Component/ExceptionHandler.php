65eddf936b6cc127c1d19fbd01929547570bb041
Raymond <torhc17311@gmail.com>
2026-08-25 19:59:32 +0900

    modify exception handler placement

---
+++ app/Component/ExceptionHandler.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+namespace Frrame\Component;
+class ExceptionHandler{
+	public static function setHandler():void{
+		if($_ENV['APP_DEBUG'] === '1' && $_ENV['APP_PROD'] === '0'){
+			ini_set('display_errors','1');
+			ini_set('display_startup_errors','1');
+			error_reporting(E_ALL);
+		}
+		if(($_ENV['APP_PROD'] === '1')){
+			ob_start();
+			ini_set('display_errors','0');
+			ini_set('display_startup_errors','0');
+			error_reporting(0);
+			// Set warnings as Exception as well.
+			set_error_handler(function($errno, $errstr, $errfile, $errline){
+				throw new \ErrorException($errstr, $errno, E_ALL, $errfile, $errline);
+			});
+			set_exception_handler(function($exception){
+				ob_end_clean();
+				$log = "--- ".date('Y-m-d H:i:s',)." Uncaught Error/Exception ---\n";
+				$log .= $exception::class." thrown in ".$exception->getFile()." : ".$exception->getLine()." : ".$exception->getMessage()."\n";
+				$log .= "Trace:\n".$exception->getTraceAsString()."\n";
+				$log .= "------\n\n";
+				if(error_log($log,3,ROOT_PATH.'/log/error.log')){
+					// Application side fault
+					http_response_code(500);
+				}else{
+					// Even this handler failed.
+					http_response_code(503);
+				}
+				exit;
+			});
+		}
+	}
+}
--- app/Facade/MiddlewareFacade.php
+++ app/Facade/MiddlewareFacade.php
@@ -1,10 +1,12 @@
 <?php declare(strict_types=1);
 namespace Frrame\Facade;
+use Frrame\Component\ExceptionHandler;
 use Frrame\Component\Http\RequestBody;
 use Frrame\Component\Http\RequestHeader;
 use Frrame\Component\Session;
 class MiddlewareFacade{
 	public static function web():void{
+		ExceptionHandler::setHandler();
 		RequestHeader::load();
 		RequestBody::load();
 		Session::load();
