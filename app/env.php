7e105135c68066ede0549eaa04f0adad3094c9e6
Raymond <torhc17311@gmail.com>
2026-08-31 00:06:13 +0900

    upd: env file handling

--- app/env.php
+++ app/env.php
@@ -1,5 +1,10 @@
 <?php declare(strict_types=1);
 # Load `.env` to global
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->load();
-$dotenv->required(['APP_PROD', 'APP_DEBUG'])->allowedValues(['0', '1']);
+try{
+	$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+	$dotenv->load();
+	$dotenv->required(['APP_PROD', 'APP_DEBUG'])->allowedValues(['0', '1']);
+}catch(\Throwable $t){
+	http_response_code(500);
+	echo 'Cannot load environment variables.';
+}
